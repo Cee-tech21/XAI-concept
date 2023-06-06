ef1165d01b406c37ffce6d60a6b1ab14ebc62503
cee-tech21 <easyoandi@gmail.com>
2023-06-06 02:21:58 +0100

    first project commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Explainable AI concept 
+Use an AI model to generate predictions on the backend and then present results for visualisation in the front end.
+
+Visit page here: https://
---
+++ javascript/index.js
@@ -0,0 +1,75 @@
+let allPosts = theData;
+
+callApiCreateArticles()
+
+console.log(allPosts[0]);
+// console.log(allPosts[0].canonical_url)
+// console.log(allPosts[0].content.rendered)
+// console.log("image url: ", allPosts[0].jetpack_featured_media_url)
+
+// Get DOM elements
+let theHeadline = getElement(".headline")
+let headlineImg = getElement("#headline-image")
+let featuredNews = getElement("#featured-News")
+
+
+// Populate DOM elements with API data
+async function callApiCreateArticles(){
+    let theApiData = await makeApiCall();
+
+    console.log("latest api data: ", theApiData)
+    let headlineCreated = await createHeadlineNews(theApiData)
+    let featuredCreated = await createFeaturedNews(theApiData)
+}
+
+// headline article
+async function createHeadlineNews(freshData){
+    theHeadline.innerHTML = `<h2 class="pb-half">
+                             ${freshData[0].title.rendered} 
+                             </h2>`
+    theHeadline.innerHTML += freshData[0].excerpt.rendered
+    theHeadline.innerHTML += `<div class="pt-half"> 
+                                <a href="${freshData[0].guid.rendered}"
+                                            class="fw-bold-one">
+                                        Read full >> 
+                                </a>
+                            </div>`
+    headlineImg.src = freshData[0].jetpack_featured_media_url
+}
+
+// Work with remaining articles for featured news
+async function createFeaturedNews(freshData){
+    for(let i = 1; i < allPosts.length; i++){
+    // console.log(allPosts[i].excerpt.rendered)
+    featuredNews.innerHTML += `<article class="card">
+                                <img src="${freshData[i].jetpack_featured_media_url}" 
+                                    class="featured__image" alt="article image">
+                                <p class="pt-half fw-bold-thin 
+                                    flex-grow-half">
+                                    ${freshData[i].title.rendered} 
+                                </p>
+                                <div class="pt-half pb-half 
+                                    flex-grow-1">
+                                    ${freshData[i].excerpt.rendered}
+                                </div> 
+
+                                <div> <a href="${freshData[i].guid.rendered}"
+                                            class="fw-bold-thin">
+                                        Read full >> 
+                                    </a>
+                                </div>
+                            </article>`
+    }
+}
+
+
+async function makeApiCall() {
+    let response = await fetch("https://techcrunch.com/wp-json/wp/v2/posts");  
+    let data = await response.json()
+    return data
+}
+
+// Function to get DOM elements
+function getElement(ident){
+    return document.querySelector(ident)
+}
